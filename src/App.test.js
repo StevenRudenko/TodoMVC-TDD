7c4b5c3b47ad520325b7ec0919c13d5636e3ff92
Stepan Rudenko <rude@Stepans-MacBook-Pro.local>
2021-12-13 18:09:51 +0200

    2.1 New task input RED

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const setup = () => {
+  const utils = render(<App />);
+  const input = screen.queryByTestId("task-input");
+  return {
+    input,
+    ...utils,
+  };
+};
+
+test("render task input", () => {
+  const { input } = setup();
+  expect(input).toBeInTheDocument();
 });
