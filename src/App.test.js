a3ea582b9eb55864d6bc5011bc52ddb8af0d5012
Stepan Rudenko <stepan.rudenko@agileengine.com>
2021-12-13 18:22:27 +0200

    6.1 Input text is cleared on new task added RED

--- src/App.test.js
+++ src/App.test.js
@@ -38,3 +38,10 @@ test("task text generated is valid", () => {
   const { tasks } = refresh();
   expect(tasks[0].textContent).toBe(taskText);
 });
+
+test("input should be cleared after new task added", () => {
+  const { input, plusButton } = setup();
+  userEvent.type(input, "new task");
+  userEvent.click(plusButton);
+  expect(input.value).toBe("");
+});
